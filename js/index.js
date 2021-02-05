ca94474fcbc6eb2969178e9aa07bb6b7c75bbdb3
Alec Scheele <alec.d.scheele@gmail.com>
2021-02-05 12:03:00 -0700

    added js
    linked pages using js

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+/*Checker for js*/
+console.log("Your index.js file is loaded correctly!");
+/*-------------------------------------------------------------*/
+
+
+/*Button animation&transition*/
+$(".aboutButton").on("click", function(){
+   console.log("You clicked me!");
+
+   setTimeout(function() {
+    $(location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/skills.html").addClass('fadeIn');
+  }, 1500);
+    $('.button').addClass('animate_content');
+   /*location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/skills.html";*/
+
+    });
+   
+$(".portButton").on("click", function(){
+    console.log("You clicked me!");
+    location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/myWork.html";
+    });
+
+$(".connectButton").on("click", function(){
+    console.log("You clicked me!");
+    location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/skills.html";
+    });
+        
+$(".intoButton").on("click", function(){
+    console.log("You clicked me!");
+    location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/skills.html";
+    });        
+
+/*------------------------------------------------------------------------------*/
+
