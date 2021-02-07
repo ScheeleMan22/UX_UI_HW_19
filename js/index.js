edbfdfc726ee30511598c3fb14424c7f9969d47d
Alec Scheele <alec.d.scheele@gmail.com>
2021-02-06 17:01:03 -0700

    Update index.js
    added a console check

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 console.log("Your index.js file is loaded correctly!");
 /*-------------------------------------------------------------*/
 
-
+/*--------------WHEN CLICKING----------------------------*/
 /*Button animation&transition*/
 $(".aboutButton").on("click", function(){
    console.log("You clicked me!");
@@ -31,9 +31,42 @@ $(".intoButton").on("click", function(){
     location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/gettingInto.html";
     });        
 
-    $(".logo").on("click", function(){
+$(".logo").on("click", function(){
         console.log("You clicked me!");
         location.href = "file:///Users/Alec/Desktop/Code/UX_UI_HW_19/index.html";
         });   
 /*------------------------------------------------------*/
+/*--------------Hovering--------------------*/
+/*$(".portButton").hover(function(){
+    console.log("You're hovering!");
+    $(".portButton").toggleClass("active");
+    if($(".portButton").hasClass("active")) {
+        $(".portButton").css("background-color", "#000000");
+    }
+        });
 
+*/
+$(".portButton").hover(function(){
+            $(this).css("background-color", "grey");
+            }, function(){
+            $(this).css("background-color", "#C38686");
+            console.log("You're hovering!");
+          });
+$(".aboutButton").hover(function(){
+            $(this).css("background-color", "grey");
+            }, function(){
+            $(this).css("background-color", "#6AA5B8");
+            console.log("You're hovering!");
+          });
+$(".connectButton").hover(function(){
+            $(this).css("background-color", "grey");
+            }, function(){
+            $(this).css("background-color", "#D6A988");
+            console.log("You're hovering!");
+          });
+$(".intoButton").hover(function(){
+            $(this).css("background-color", "grey");
+            }, function(){
+            $(this).css("background-color", "#9276BF");
+            console.log("You're hovering!");
+          });
